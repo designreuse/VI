2adb6f62cb499cc44b8f45cd7f997ec6f72a2605
yapyilin <smu@YYLs-MacBook-Pro.local>
2015-08-31 14:37:24 +0800

    Testing git

--- src/testing/test.java
+++ src/testing/test.java
@@ -3,6 +3,7 @@ package testing;
 import model.Admin;
 import dataManager.AdminDAO;
 
+//this is a test comment
 public class test {
 	public static void main(String[] args){
 	Admin admin = new Admin();
