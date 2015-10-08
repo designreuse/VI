26c77101137b7479dcd1885cac3810b143b7441b
xinyinglee <xinying.lee.2013@sis.smu.edu.sg>
2015-10-08 17:10:14 +0800

    Front End Integration: edit function for parent.
    Working edit functionality for parent.

--- WebContent/js/branchmanager/parent.js
+++ WebContent/js/branchmanager/parent.js
@@ -7,8 +7,6 @@
     });
 //}
 
-var PARENTS = [];
-
 function registerParent() {
 	var parentEmail = $("#parentEmail").val();
 	var parentPassword = $("#parentPassword").val();
@@ -49,57 +47,110 @@ function registerParent() {
 }
 
 function getParents() {
-	$.ajax({
-		url : '../VI/GetAllParentsServlet?input={}',
-		method : 'GET',
-		dataType : 'json',
-		error : function(err) {
-			console.log(err);
-		},
-		success : function(data) {
-			if (data.status == 1){
-				//loadDataTable();
-			}
-			else{
-				console.log(data.message);
-			}
-		}
-	});		
+	$.fn.dataTable.ext.errMode = 'none';
+	
+	var table =  $('#parentTable').on( 'error.dt', function ( e, settings, techNote, message ) {
+	        console.log( 'An error has been reported by DataTables: ', message );
+	    }).DataTable({
+	    	ajax:{
+				 url: '../VI/GetAllParentsServlet?input={}',
+				 dataSrc: 'message'
+			 },
+			 columns: [
+				 {"data": "parentId"},
+				 {"data": "remark"},
+				 {"data": 'name'},
+				 {"data": 'contact'},
+				 {"data": 'email'},
+				 {"data": 'address'},
+				 {"data": 'createDate'},
+				 {"data": null, "defaultContent":'<button class="btn btn-sm btn-success fa fa-file-powerpoint-o" title=""></button>'}
+				]
+	    });
+	
+		 
+		$('#parentTable tbody').on( 'click', 'td', function () {
+			var tr = $(this).closest('tr');
+	        var row = table.row( tr );
+	       
+	        bootbox.dialog({
+	        	title: "Edit Details",
+	    		message: '<div class="row">  ' +
+				'<div class="col-md-12"> ' +
+				'<form class="form-horizontal" method="post"> ' +
+				'<div class="form-group"> ' +
+				'<div><font color="red" id="updatedMessage"></font></div>'+
+				'<label class="col-md-2 control-label" for="name">Name</label> ' +
+				'<div class="col-md-10"> ' +
+				'<input id="name" name="name" type="text" class="form-control input-md" placeholder= ' + row.data().name + '> ' +
+				'</div> ' +
+				'<label class="col-md-2 control-label" for="contact">Contact</label> ' +
+				'<div class="col-md-10"> ' +
+				'<input id="contact" name="contact" type="text" class="form-control input-md" placeholder= ' + row.data().contact + '> ' +
+				'</div> ' +
+				'<label class="col-md-2 control-label" for="email">Email</label> ' +
+				'<div class="col-md-10"> ' +
+				'<input id="email" name="email" type="text" class="form-control input-md" placeholder= ' + row.data().email + '> ' +
+				'</div> ' +
+				'<label class="col-md-2 control-label" for="address">Address</label> ' +
+				'<div class="col-md-10"> ' +
+				'<input id="address" name="address" type="text" class="form-control input-md" placeholder= ' + row.data().address + '> ' +
+				'</div> ' +
+				'</div> ' +
+				'</form> ' +
+				'</div> ' +
+				'</div>',
+	    		onEscape: function() {
+	    			console.log("this got cancelled");
+	    		},
+	    		buttons: {
+	    			success:{
+	    				label: "Save!",
+	    				className: "btn-success",
+	    				callback: function(){
+	    					var parentId = row.data().parentId;
+	    					var updatedName = $("#name").val();
+	    					var updatedContact = $("#contact").val();
+	    					var updatedAddress = $("#address").val();
+	    					var updatedEmail = $("#email").val();
+	    					
+	    					var input = {}
+	    					input.parentId = Number(parentId);
+	    					input.name = updatedName;
+	    					input.contact = updatedContact;
+	    					input.address = updatedAddress;
+	    					input.email = updatedEmail;
+	    					var inputStr = JSON.stringify(input);
+	    					
+	    					inputStr = encodeURIComponent(inputStr);
+	    					$.ajax({
+	    						url : '../VI/UpdateParentServlet?input=' + inputStr, //this part sends to the servlet
+	    						method : 'POST',
+	    						dataType : 'json',
+	    						error : function(err) {
+	    							console.log(err);
+	    							$("#message").html("System has some error. Please try again.");
+	    						},
+	    						success : function(data) {
+	    							console.log(data);
+	    							var status = data.status; 
+	    							var message = data.message;
+	    							
+	    							if (status == 1) {
+	    								console.log("successful update!");
+	    								table.draw();
+	    							} else {
+	    								$("#message").html("Something's wrong, please try again!");
+	    							}
+	    						}
+	    					});
+	    				}
+	    			}
+	    		}	
+	    	}); 
+		});
+		
 }
 
-function loadDataTable(){
-//	$('#parentTable').DataTable({
-//		ajax:{
-//			url: '../VI/GetAllParentsServlet?input={}',
-//			dataSrc: 'message'
-//		},
-//		columns: [
-//			{"data": "parentId"},
-//			{"data": "remark"},
-//			{"data": 'name'},
-//			{"data": 'contact'},
-//			{"data": 'email'},
-//			{"data": 'address'},
-//			{"data": 'createDate'},
-//			{"data": null, "defaultContent":'<button class="btn btn-sm btn-success fa fa-file-powerpoint-o" onclick="test();" title=""></button>'}
-//		]
-//	});
-}
-
-function test(){
-	bootbox.dialog({
-		message: "modal popup example!",
-		title: "Modal Pop-up Example",
-		onEscape: function() {},
-		buttons: {
-			success:{
-				label: "Success!",
-				className: "btn-success",
-				callback: function(){}
-			}
-		}	
-	});
-
-}
 
 
