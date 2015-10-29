8074f8c8d78827243c76702c2f812815c27801ab
xinyinglee <xinying.lee.2013@sis.smu.edu.sg>
2015-10-30 01:42:13 +0800

    Front-end Integration: BM view calendar (incomplete)
    - updated web.xml due to duplicates in url mapping
    - updated calendar js with some ajax calls.

--- WebContent/js/branchmanager/calendar.js
+++ WebContent/js/branchmanager/calendar.js
@@ -1,4 +1,70 @@
 $(document).ready(function() {
+	branchCourseCall();
+//	calendarInitiate()
+});
+
+function branchCourseCall(){
+	var branchId = Number(localStorage.getItem("branchId"));
+	var input = {};
+	input.branchId = branchId
+	var inputStr = JSON.stringify(input);
+	inputStr = encodeURIComponent(inputStr);
+	$.ajax({
+		url : '../VI/GetBranchCoursesByBranchServlet?input=' + inputStr, //this part sends to the servlet
+		method : 'POST',
+		dataType : 'json',
+		error : function(err) {
+			console.log(err);
+		},
+		success : function(data) {
+			// you have to do a for loop to get all the data.'
+			console.log(data);
+			var status = data.status; 
+			var message = data.message;
+			
+			if (status == 1) {
+				for (var i = 0; i < message.length; i++){
+					var obj = message[i];
+					var id = obj.course.courseId;
+					scheduleServletCall(id);
+				}		
+			} else {
+				console.log("try agin");
+			}
+		}
+	});
+}
+
+function scheduleServletCall(id){
+	var courseId = Number(id);
+	var input = {};
+	input.courseId = courseId
+	var inputStr = JSON.stringify(input);
+	inputStr = encodeURIComponent(inputStr);
+	
+	$.ajax({
+		url : '../VI/GetSchedulesByCourse?input=' + inputStr, //this part sends to the servlet
+		method : 'POST',
+		dataType : 'json',
+		error : function(err) {
+			console.log(err);
+		},
+		success : function(data) {
+			var status = data.status; 
+			var message = data.message;
+			
+			if (status == 1) {
+				console.log(JSON.stringify(message));
+				//use for loop to create new json object and send it back to previous call.
+			} else {
+				console.log("try agin");
+			}
+		}
+	});
+	
+}
+
+function calendarInitiate(){
 	var date = new Date();
 	var d = date.getDate();
 	var m = date.getMonth();
@@ -18,27 +84,8 @@ $(document).ready(function() {
                     start: new Date(y, m, 17, 12, 00),
                     end: new Date(y, m, 17, 18, 00),
                     allDay: false
-				},
-				{
-					title: 'SP Tourism: PM Shift',
-                    start: new Date(y, m, 24, 12, 00),
-                    end: new Date(y, m, 24, 18, 00),
-                    allDay: false
-				} ,
-				{
-					title: 'SP Tourism: PM Shift',
-                    start: new Date(y, m, 25, 8, 00),
-                    end: new Date(y, m, 25, 13, 00),
-                    allDay: false
-				} ,
-				{
-					title: 'SP Tourism: AM Shift',
-                    start: new Date(y, m, 31, 8, 00),
-                    end: new Date(y, m, 31, 13, 00),
-                    allDay: false
 				}
 			]
 
 		});
-		
-});
+}
