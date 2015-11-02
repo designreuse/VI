c7bac79f4fbfcd878ace4650ad07873793865dbe
yapyilin <yilin.yap.2013@sis.smu.edu.sg>
2015-11-02 19:16:21 +0800

    .

--- WebContent/js/form-validation-script.js
+++ WebContent/js/form-validation-script.js
@@ -1,95 +1,52 @@
-var Script = function () {
-
-    $.validator.setDefaults({
-        submitHandler: function() { alert("submitted!"); }
-    });
-
-    $().ready(function() {
-        // validate the comment form when it is submitted
-        $("#feedback_form").validate();
-
-        // validate signup form on keyup and submit
-        $("#register_form").validate({
-            rules: {
-                fullname: {
-                    required: true,
-                    minlength: 6
-                },
-                address: {
-                    required: true,
-                    minlength: 10
-                },
-                username: {
-                    required: true,
-                    minlength: 5
-                },
-                password: {
-                    required: true,
-                    minlength: 5
-                },
-                confirm_password: {
-                    required: true,
-                    minlength: 5,
-                    equalTo: "#password"
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                topic: {
-                    required: "#newsletter:checked",
-                    minlength: 2
-                },
-                agree: "required"
-            },
-            messages: {                
-                fullname: {
-                    required: "Please enter a Full Name.",
-                    minlength: "Your Full Name must consist of at least 6 characters long."
-                },
-                address: {
-                    required: "Please enter a Address.",
-                    minlength: "Your Address must consist of at least 10 characters long."
-                },
-                username: {
-                    required: "Please enter a Username.",
-                    minlength: "Your username must consist of at least 5 characters long."
-                },
-                password: {
-                    required: "Please provide a password.",
-                    minlength: "Your password must be at least 5 characters long."
-                },
-                confirm_password: {
-                    required: "Please provide a password.",
-                    minlength: "Your password must be at least 5 characters long.",
-                    equalTo: "Please enter the same password as above."
-                },
-                email: "Please enter a valid email address.",
-                agree: "Please accept our terms & condition."
-            }
-        });
-
-        // propose username by combining first- and lastname
-        $("#username").focus(function() {
-            var firstname = $("#firstname").val();
-            var lastname = $("#lastname").val();
-            if(firstname && lastname && !this.value) {
-                this.value = firstname + "." + lastname;
-            }
-        });
-
-        //code to hide topic selection, disable for demo
-        var newsletter = $("#newsletter");
-        // newsletter topics are optional, hide at first
-        var inital = newsletter.is(":checked");
-        var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
-        var topicInputs = topics.find("input").attr("disabled", !inital);
-        // show when newsletter is checked
-        newsletter.click(function() {
-            topics[this.checked ? "removeClass" : "addClass"]("gray");
-            topicInputs.attr("disabled", !this.checked);
-        });
-    });
-
-
-}();
+ $().ready(function() {
+	$('#register_form').validate({
+		rules : {
+			parentName : {
+				required : true,
+				minlength : 2,
+				maxlength : 20,
+				lettersonly : true
+			},
+			parentNric : {
+				required : true,
+				minlength : 9,
+				maxlength : 9,
+				nowhitespace : true
+			},
+			contactNumber : {
+				required : true,
+				minlength : 10,
+				maxlength : 13,
+				digits : true
+			},
+			parentAddress : {
+				required : true,
+				minlength : 10,
+			}
+		},
+		messages : {
+			parentName : {
+				required : "Please enter your name",
+				minlength : "Name should be more than 2 characters",
+				maxlength : "Name should be less than 20 characters",
+				lettersonly : "Name should contain only letters"
+			},
+			parentNric : {
+				required : "Please enter your NRIC",
+				minlength : "NRIC should be more than 9 characters",
+				maxlength : "NRIC should be less than 9 characters",
+				nowhitespace : "NRIC should not have any spaces"
+			},
+			contactNumber : {
+				required : "Please enter your mobile number",
+				minlength : "Mobile number should be more than 10 characters",
+				maxlength : "Mobile number should be less than 13 characters",
+				digits : "Mobile number should contain only digits"
+			},
+			parentAddress : {
+				required : "Please enter your address",
+				minlength : "Address should be more than 10 characters",
+			}
+		},
+	});
+});
